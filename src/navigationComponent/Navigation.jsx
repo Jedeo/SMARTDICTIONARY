c93261f52998949acf729e0714e84a135eda3d39
Jedeo <jedeopastor@gmail.com>
2022-11-10 19:27:50 -0700

    Add: Props for learning

--- src/navigationComponent/Navigation.jsx
+++ src/navigationComponent/Navigation.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Navigation.css"
 
-export default function Navigation() {
+export default function Navigation({startNewRound}) {
   return (
     <nav className="navbar">
       <Link className="home-button" to="/"> HOME </Link>
       <div>
       <Link className="word-of-the-day-button" to="/wordOfTheDay"> WORD OF THE DAY </Link>
-      <Link to="/quiz"className="learn-button"> LEARN </Link> 
+      <Link to="/quiz"className="learn-button" onClick={()=> startNewRound(true)}> LEARN </Link> 
       </div>
     </nav>
   );
