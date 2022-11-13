b0e8046a2c8cf8bc83a645179cbe9bef353d8231
Jedeo <jedeopastor@gmail.com>
2022-11-12 18:36:47 -0700

    Add: word of the day to home page

--- src/components/HomeComponent/Home.jsx
+++ src/components/HomeComponent/Home.jsx
@@ -1,8 +1,23 @@
 import Form from "../formComponent/Form";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { wordOfTheDay } from "../../apiCalls/getWords";
+import "./Home.css"
 
 const Home = () => {
+const [todayWord, setTodayWord] = useState("")
+useEffect(()=>{
+const requestWord = async ()=> {
+ const result = await wordOfTheDay()
+ setTodayWord(result.word)
+}
+requestWord()
+},[])
+
     return(
-        <div>
+        <div className="home-container">
+            <h1 className="discover"> Discover today's word</h1>
+            <Link to="/wordOfTheDay" className="todays-word"> {todayWord} </Link>
             <Form/>
         </div>
     )
